13dd1e4d783678661d903224dc5adb823cfef887
CaoMeiYouRen <996881204@qq.com>
2021-03-03 23:18:48 +0800

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// const __ERROR__ = process.env.NODE_ENV === 'production' ? 2 : 0
+const __WARN__ = process.env.NODE_ENV === 'production' ? 1 : 0
+module.exports = {
+    root: true,
+    globals: {
+    },
+    env: {
+    },
+    settings: {
+    },
+    extends: [
+        'cmyr',
+    ],
+    plugins: [
+    ],
+    rules: {
+        'no-console': __WARN__,
+        '@typescript-eslint/explicit-module-boundary-types': [1, {
+            allowArgumentsExplicitlyTypedAsAny: true,
+        }], // 要求导出函数和类的公共类方法的显式返回和参数类型
+    },
+}
